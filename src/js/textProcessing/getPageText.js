9495dc9ac8326e3b4c80d1d231849a96f7541100
hughugh <lsjppp8069@kookmin.ac.kr>
2021-03-17 09:19:42 +0900

    filter current page with removing useless tags

--- src/js/textProcessing/getPageText.js
+++ src/js/textProcessing/getPageText.js
@@ -1,9 +1,34 @@
-function filterMainContent() {
+function removeTag(copy, tagName) {
+  let tags = copy.getElementsByTagName(tagName);
+  let l = tags.length;
+  let i;
+  for (i = l - 1; i >= 0; i--) {
+    tags[i].parentNode.removeChild(tags[i]);
+  }
+  return copy;
+}
+
+function filterMainContent(copy) {
   // 핵심 내용에 해당하는 text를 필터링 하는 함수
   // 관련된 함수가 많이 필요할 것.
 
   // test code
-  return 'Hi.';
+  return copy.body.innerText;
+}
+
+function getTitle() {
+  return document.getElementsByTagName('title')[0].innerText;
+}
+
+function sendMainContent(text, title) {
+  // 핵심 내용을 백엔드쪽(background.js)에 보내는 역할
+
+  let message = {
+    type: 'text',
+    text: text,
+    title: title
+  };
+  chrome.runtime.sendMessage(message, HandleSendMainContentResponse);
 }
 
 function HandleSendMainContentResponse(response) {
@@ -15,24 +40,22 @@ function HandleSendMainContentResponse(response) {
   console.log(response);
 }
 
-function sendMainContent(text) {
-  // 핵심 내용을 백엔드쪽(background.js)에 보내는 역할
-
-  let message = {
-    type: 'text',
-    text: text
-  };
-  chrome.runtime.sendMessage(message, HandleSendMainContentResponse);
-}
-
 function main() {
   // 현재 페이지에서 메인에 해당하는 text 를 불러오기 위한 함수
   // 현재 페이지에 접근하고, 필터 함수에 전달하고, 그 핵심 내용을 받아 다시
   // 전달하는 역할
 
   // test code
-  let mainText = filterMainContent();
-  sendMainContent(mainText);
+  let title = getTitle();
+  let copy = document.cloneNode(true);
+  let tags = ['a', 'code', 'script', 'style'];
+  tags.forEach(function (tag) {
+    copy = removeTag(copy, tag);
+  });
+  let mainText = filterMainContent(copy);
+  console.log(title);
+  console.log(mainText);
+  //sendMainContent(mainText);
 }
 
 main();
