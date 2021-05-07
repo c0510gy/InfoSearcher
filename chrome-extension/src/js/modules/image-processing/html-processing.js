773de43a4b9d2a2f7a1e950eea73e5975621c1c7
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-07 13:55:42 +0900

    feat: impelement image extraction & convert from uri to base64 encoded image

---
+++ chrome-extension/src/js/modules/image-processing/html-processing.js
@@ -0,0 +1,29 @@
+function getBase64Image(imageSrc) {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const img = new Image();
+    img.src = imageSrc;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+      canvas.width = this.width;
+      canvas.height = this.height;
+      ctx.drawImage(img, 0, 0);
+      resolve(canvas.toDataURL());
+    };
+  });
+}
+
+export default async function imageProcessingFromHTML(document) {
+  const images = [];
+  Array.from(document.images).forEach(async function (imageElem) {
+    try {
+      images.push({
+        base64: await getBase64Image(imageElem.src),
+        src: imageElem.src
+      });
+    } catch (err) {}
+  });
+  console.log('images', images);
+}
