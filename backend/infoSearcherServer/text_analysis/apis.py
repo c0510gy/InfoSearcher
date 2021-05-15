51dd91e438d79f227d4c9db0693a74fb0777c8c5
hughugh <lsjppp8069@kookmin.ac.kr>
2021-05-15 17:41:55 +0900

    feat: text analysis with solynlp

--- backend/infoSearcherServer/text_analysis/apis.py
+++ backend/infoSearcherServer/text_analysis/apis.py
@@ -1,6 +1,12 @@
 import json
 import urllib.request
 
+from soynlp.utils import DoublespaceLineCorpus
+from soynlp.noun import LRNounExtractor_v2
+from soynlp.word import WordExtractor
+from soynlp.tokenizer import LTokenizer
+from krwordrank.word import KRWordRank
+
 
 def search_in_naver(search_words, field='blog'):
     valid_fields = [
@@ -42,10 +48,96 @@ def search_in_naver(search_words, field='blog'):
         return rescode, 'Error from naver'
 
 
-if __name__ == '__main__':
-    test_word_list = ['언어_모델', '자연어_처리', '형태소_분석', '말뭉치_구축', '언어', '기술', '모델']
-    test_word_list = [i.replace('_', '') for i in test_word_list]
-    test_word_list = ' '.join(test_word_list)
-    rescode, result = search_in_naver(test_word_list, 'blog')
-    print(rescode)
-    print(result)
+def get_tokenizer(sentences):
+    word_extractor = WordExtractor()
+
+    word_extractor.train(sentences)
+    words = word_extractor.extract()
+    cohesion_score = {word:score.cohesion_forward for word, score in words.items()}
+
+    noun_extractor = LRNounExtractor_v2()
+    nouns = noun_extractor.train_extract(sentences) # list of str like
+
+    noun_scores = {noun:score.score for noun, score in nouns.items()}
+    combined_scores = {noun:score + cohesion_score.get(noun, 0)
+        for noun, score in noun_scores.items()}
+
+    tokenizer = LTokenizer(scores=combined_scores)
+
+    return noun_extractor, nouns, tokenizer
+
+
+def get_tokenized_nouns(sentences, noun_extractor, nouns, tokenizer):
+    tokenized_sentences = []
+    for sent in sentences:
+        tokenized_sentences.append(tokenizer.tokenize(sent))
+    compound_nouns = {}
+    for compound_noun, sub_nouns in noun_extractor._compounds_components.items():
+        compound_nouns[compound_noun] = list(sub_nouns)
+
+    tokenized_nouns = []
+    for sent in tokenized_sentences:
+        temp_sents = []
+        for word in sent:
+            if (word in nouns) and (word not in compound_nouns) and (len(word) > 1):
+                temp_sents.append(word)
+            elif word in compound_nouns:
+                temp_sents.extend(compound_nouns[word])
+        tokenized_nouns.append(temp_sents)
+
+    tokenized_nouns = [nouns for nouns in tokenized_nouns if len(nouns) > 3]
+
+    return tokenized_nouns
+
+
+def get_2gram(sentences):
+    ngram_sentences = []
+
+    for sentence in sentences:
+        ngrams = []
+        for idx in range(len(sentence) - 1):
+            ngrams.append('{}_{}'.format(sentence[idx], sentence[idx + 1]))
+        ngram_sentences.append(ngrams)
+
+    return ngram_sentences
+
+
+def wordrank(words):
+    min_count = 5  # 단어의 최소 출현 빈도수 (그래프 생성 시)
+    max_length = 12  # 단어의 최대 길이
+    wordrank_extractor = KRWordRank(min_count=min_count, max_length=max_length)
+
+    beta = 0.85  # PageRank의 decaying factor beta
+    max_iter = 30
+    keywords, rank, graph = wordrank_extractor.extract(words, beta, max_iter)
+    return keywords
+
+
+def filter_two_gram(keywords, n):
+    ret = []
+    for keyword, score in keywords.items():
+        if keyword[-1] != '_' and keyword.find('_') != -1:
+            ret.append((keyword, score))
+    return ret[:n]
+
+
+def filter_one_gram(keywords, n):
+    ret = [(keyword, score) for keyword, score in keywords.items()]
+    return ret[:n]
+
+
+def get_keywords(sentences):
+    # Get nouns and tokenizer
+    noun_extractor, nouns, tokenizer = get_tokenizer(sentences)
+
+    # Get nouns
+    tokenized_nouns = get_tokenized_nouns(sentences, noun_extractor, nouns, tokenizer)
+    one_gram_nouns = [' '.join(nouns) for nouns in tokenized_nouns]
+    two_gram_nouns = get_2gram(tokenized_nouns)
+    two_gram_nouns = [' '.join(nouns) for nouns in two_gram_nouns]
+
+    # Get keywords
+    one_gram_keywords = filter_one_gram(wordrank(one_gram_nouns), 2)
+    two_gram_keywords = filter_two_gram(wordrank(two_gram_nouns), 3)
+
+    return [keywordInfo[0] for keywordInfo in one_gram_keywords + two_gram_keywords]
--- backend/infoSearcherServer/text_analysis/views.py
+++ backend/infoSearcherServer/text_analysis/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .apis import search_in_naver
+from .apis import search_in_naver, get_keywords
 
 
 class TextAnalysisView(APIView):
@@ -14,12 +14,16 @@ class TextAnalysisView(APIView):
         print(data)
 
         # 키워드 추출
-        test_word_list = ['언어_모델', '자연어_처리', '형태소_분석', '말뭉치_구축', '언어', '기술', '모델']
-        test_word_list = [i.replace('_', '') for i in test_word_list]
-        test_word_list = ' '.join(test_word_list)
+        keywords = get_keywords(data['content'])
+        if len(keywords) == 0:
+            return Response({
+                'message': 'Content is too short.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        keywords = ' '.join([keyword.replace('_', '') for keyword in keywords])
 
         # 네이버 API 호출
-        rescode, result = search_in_naver(test_word_list, 'blog')
+        rescode, result = search_in_naver(keywords, 'blog')
 
         # 키워드를 추출하고, 네이버에 검색한 다음에, 결과에서 몇개 뽑아서 링크 제공
         if rescode != 200:
