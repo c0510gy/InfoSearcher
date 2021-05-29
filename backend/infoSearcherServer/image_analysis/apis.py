d76a42deb83447b152a5ad7ea0618c5aef678c1d
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-29 22:03:51 +0900

    feat: implement detect function

--- backend/infoSearcherServer/image_analysis/apis.py
+++ backend/infoSearcherServer/image_analysis/apis.py
@@ -1,5 +1,64 @@
 import cv2
+import numpy as np
 
 def load_model(weights, config):
   net = cv2.dnn.readNet(weights, config)
   return net
+
+def detect(model, image):
+  Width = image.shape[1]
+  Height = image.shape[0]
+  scale = 0.00392
+
+  blob = cv2.dnn.blobFromImage(image, scale, (608,608), (0,0,0), True, crop=False)
+
+  model.setInput(blob)
+
+  def get_output_layers(net):
+      
+      layer_names = net.getLayerNames()
+      
+      output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+      return output_layers
+
+  outs = model.forward(get_output_layers(model))
+
+  class_ids = []
+  confidences = []
+  boxes = []
+  conf_threshold = 0.5
+  nms_threshold = 0.4
+
+  for out in outs:
+      for detection in out:
+          scores = detection[5:]
+          class_id = np.argmax(scores)
+          confidence = scores[class_id]
+          if confidence > 0.5:
+              center_x = int(detection[0] * Width)
+              center_y = int(detection[1] * Height)
+              w = int(detection[2] * Width)
+              h = int(detection[3] * Height)
+              x = center_x - w / 2
+              y = center_y - h / 2
+              class_ids.append(class_id)
+              confidences.append(float(confidence))
+              boxes.append([x, y, w, h])
+
+  indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+  result = []
+
+
+  for i in indices:
+      i = i[0]
+      box = boxes[i]
+      x = box[0]
+      y = box[1]
+      w = box[2]
+      h = box[3]
+
+      result.append([class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h)])
+
+  return result
