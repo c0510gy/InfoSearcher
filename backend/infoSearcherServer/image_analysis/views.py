2a04b0a8d84dceb724490d8d68eed707d9e56652
Sang-geon Yun <c0510gy@protonmail.ch>
2021-05-29 22:20:19 +0900

    fix: fix file names

--- backend/infoSearcherServer/image_analysis/views.py
+++ backend/infoSearcherServer/image_analysis/views.py
@@ -1,7 +1,20 @@
-from django.urls import path
-from .views import ImageAnalysisView
+import json
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .apis import YOLOV3_MODEL, detect
 
 
-urlpatterns = [
-    path('', ImageAnalysisView.as_view()),
-]
+class ImageAnalysisView(APIView):
+    def get(self, request):
+        data = json.loads(request.body)
+
+        image = data['image']
+
+        detected = detect(YOLOV3_MODEL, image)
+
+        return Response({
+                  'detected': detected
+              }, status=status.HTTP_200_OK)
